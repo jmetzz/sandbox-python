67e97d62194b260a91e94e1699c043411d63e47f
Jean Metz <j.metz@ryte.com>
2022-05-27 20:05:57 +0200

    Resolve Assert statement used outside of tests BAN-B101

--- profiling/src/algos/juliaset/juliaset_cy.py
+++ profiling/src/algos/juliaset/juliaset_cy.py
@@ -45,7 +45,8 @@ def calc_pure_python(desired_width, max_iterations):
     print(f"Took {secs:0.2f} seconds")
     # This sum is expected for a 1000^2 grid with 300 iterations
     # It ensures that our code evolves exactly as we'd intended
-    assert sum(output) == 33219980
+    if sum(output) != 33219980:
+        raise AssertionError("Calculation error")
 
 
 if __name__ == "__main__":
