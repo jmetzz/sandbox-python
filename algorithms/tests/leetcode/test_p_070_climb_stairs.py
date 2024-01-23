c28ac58e2a5f22887ca2661a647f90563200ed12
Jean Metz <jmetzz@gmail.com>
2024-01-23 13:58:47 +0100

    Add leetcode P 070

---
+++ algorithms/tests/leetcode/test_p_070_climb_stairs.py
@@ -0,0 +1,35 @@
+import pytest
+
+from leetcode.p_070_climb_stairs import climb_stairs_memoization, climb_stairs_tabulation, \
+    climb_stairs_space_optimization
+
+
+
+@pytest.mark.parametrize("test_input, expected",
+                         [(0, 1),
+                          (1, 1),
+                          (2, 2),
+                          (3, 3),
+                          (4, 5)])
+def test_climb_stairs_memoization(test_input, expected):
+    assert climb_stairs_memoization(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input, expected",
+                         [(0, 1),
+                          (1, 1),
+                          (2, 2),
+                          (3, 3),
+                          (4, 5)])
+def test_climb_stairs_tabulation(test_input, expected):
+    assert climb_stairs_tabulation(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input, expected",
+                         [(0, 1),
+                          (1, 1),
+                          (2, 2),
+                          (3, 3),
+                          (4, 5)])
+def test_climb_stairs_space_optimization(test_input, expected):
+    assert climb_stairs_space_optimization(test_input) == expected
