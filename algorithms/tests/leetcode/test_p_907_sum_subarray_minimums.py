5e968106c03a89fd8326bad971b3e2cbd6f70cab
Jean Metz <jmetzz@gmail.com>
2024-01-23 14:14:57 +0100

    Add leetcode P 907

---
+++ algorithms/tests/leetcode/test_p_907_sum_subarray_minimums.py
@@ -0,0 +1,32 @@
+import pytest
+
+from conftest import HUGE_MATRIX
+from leetcode.p_907_sum_subarray_minimums import SumSubarrayMinsNaive, SumSubarrayMinsStackingB, \
+    SumSubarrayMinsStackingA
+
+
+@pytest.mark.parametrize("test_input, expected",
+                         [
+                             ([3, 1, 2, 4], 17),
+                             ([11, 81, 94, 43, 3], 444),
+                         ])
+def test_sum_subarray_min_naive(test_input, expected):
+    assert expected == SumSubarrayMinsNaive().solve(test_input)
+
+
+@pytest.mark.parametrize("test_input, expected",
+                         [
+                             ([3, 1, 2, 4], 17),
+                             ([11, 81, 94, 43, 3], 444),
+                         ])
+def test_sum_subarray_min_stacking_a(test_input, expected):
+    assert expected == SumSubarrayMinsStackingA().solve(test_input)
+
+
+@pytest.mark.parametrize("test_input, expected",
+                         [
+                             ([3, 1, 2, 4], 17),
+                             ([11, 81, 94, 43, 3], 444),
+                         ])
+def test_sum_subarray_min_stacking_b(test_input, expected):
+    assert expected == SumSubarrayMinsStackingB().solve(test_input)
