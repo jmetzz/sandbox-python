e119f8679f3db9ebf10975cab482d113cafcf4ac
Jean Metz <jmetzz@gmail.com>
2024-01-26 10:03:45 +0100

    Add leetcode P 1143 longest common subsequence

---
+++ algorithms/tests/leetcode/test_p_1143_longest_common_subsequence.py
@@ -0,0 +1,13 @@
+import pytest
+
+from leetcode.p_1143_longest_common_subsequence import LongestCommonSubsequence
+
+
+@pytest.mark.parametrize("test_input_1, test_input_2, expected",
+                         [
+                             ("abcde", "ace", 3),
+                             ("abc", "abc", 3),
+                             ("abc", "def", 0),
+                         ])
+def test_longest_commmon_subsequence(test_input_1, test_input_2, expected):
+    assert LongestCommonSubsequence().solve(test_input_1, test_input_2) == expected
