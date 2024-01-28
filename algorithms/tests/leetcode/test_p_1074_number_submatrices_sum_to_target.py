37f789f6314a215bfd9534dee56f856b19cff125
Jean Metz <jmetzz@gmail.com>
2024-01-28 18:01:46 +0100

    Add leetcode P 1074 Number of Submatrices That Sum to Target

---
+++ algorithms/tests/leetcode/test_p_1074_number_submatrices_sum_to_target.py
@@ -0,0 +1,23 @@
+import pytest
+
+from leetcode.p_1074_number_submatrices_sum_to_target import NumSubmatricesSumTarget
+
+
+@pytest.mark.parametrize("test_input_matrix, test_input_target, expected",
+                         [
+                             ([[904]], 0, 0),
+                             ([[1, -1], [-1, 1]], 0, 5),
+                             ([[0, 1, 0], [1, 1, 1], [0, 1, 0]], 0, 4),
+                         ])
+def test_number_submatrices_sum_target_grid(test_input_matrix, test_input_target, expected):
+    assert NumSubmatricesSumTarget().solve_2d_grid(test_input_matrix, test_input_target) == expected
+
+
+@pytest.mark.parametrize("test_input_matrix, test_input_target, expected",
+                         [
+                             ([[904]], 0, 0),
+                             ([[1, -1], [-1, 1]], 0, 5),
+                             ([[0, 1, 0], [1, 1, 1], [0, 1, 0]], 0, 4),
+                         ])
+def test_number_submatrices_sum_target_hashmap(test_input_matrix, test_input_target, expected):
+    assert NumSubmatricesSumTarget().solve_hashmap(test_input_matrix, test_input_target) == expected
