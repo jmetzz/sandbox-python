07ef61a14888c07aba0a1dee96f4197bd0034388
Jean Metz <jmetzz@gmail.com>
2024-02-12 10:00:33 +0100

    Add leetcode P 169 Majority Element

--- algorithms/tests/leetcode/test_p_169_majority_element.py
+++ algorithms/tests/leetcode/test_p_169_majority_element.py
@@ -2,13 +2,14 @@ import pytest
 
 from leetcode.p_169_majority_element import MajorityElement
 
-INPUT_1 = []
-INPUT_2 = [1]
-INPUT_3 = [3, 2, 3]
-INPUT_4 = [2, 2, 1, 1, 1, 2, 2]
-INPUT_5 = [6, 6, 6, 7, 7]
-INPUT_6 = [1, 2, 3]
-INPUT_7 = [6, 6, 6, 7, 7, 7]
+INPUT_1 = []  # --> None
+INPUT_2 = [1]  # --> 1
+INPUT_3 = [3, 2, 3]  # --> 3
+INPUT_4 = [2, 2, 1, 1, 1, 2, 2]  # --> 2
+INPUT_5 = [6, 6, 6, 7, 7]  # --> None
+INPUT_6 = [1, 2, 3]  # --> None
+INPUT_7 = [6, 6, 6, 7, 7, 7]  # --> None
+INPUT_8 = [1, 2, 3, 4, 5, 5, 5]  # --> None
 
 
 @pytest.mark.parametrize("test_input, expected",
@@ -19,7 +20,8 @@ INPUT_7 = [6, 6, 6, 7, 7, 7]
                              (INPUT_4, 2),
                              (INPUT_5, 6),
                              (INPUT_6, None),
-                             (INPUT_6, None)
+                             (INPUT_7, None),
+                             (INPUT_8, None),
                          ])
 def test_majority_element_solve_counter(test_input, expected):
     assert MajorityElement().solve_counter(test_input) == expected
@@ -33,7 +35,9 @@ def test_majority_element_solve_counter(test_input, expected):
                              (INPUT_4, 2),
                              (INPUT_5, 6),
                              (INPUT_6, None),
-                             (INPUT_6, None)
+                             (INPUT_7, None),
+                             (INPUT_8, None),
+
                          ])
 def test_majority_element_solve_heap(test_input, expected):
     assert MajorityElement().solve_heap(test_input) == expected
@@ -47,7 +51,9 @@ def test_majority_element_solve_heap(test_input, expected):
                              (INPUT_4, 2),
                              (INPUT_5, 6),
                              (INPUT_6, None),
-                             (INPUT_6, None)
+                             (INPUT_7, None),
+                             (INPUT_8, None),
+
                          ])
 def test_majority_element_solve_boyer_moore(test_input, expected):
     assert MajorityElement().solve_boyer_moore_majority_alg(test_input) == expected
