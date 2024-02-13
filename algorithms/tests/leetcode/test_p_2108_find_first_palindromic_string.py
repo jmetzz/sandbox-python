ea50fe6b0c015b099caa94c2d34ee431f0ab5744
Jean Metz <jmetzz@gmail.com>
2024-02-13 08:20:28 +0100

    Add leetcode P 2108 Find first palindromic string

---
+++ algorithms/tests/leetcode/test_p_2108_find_first_palindromic_string.py
@@ -0,0 +1,14 @@
+import pytest
+
+from leetcode.p_2108_find_first_palindromic_string import FirstPalindrome
+
+
+@pytest.mark.parametrize("test_input, expected",
+                         [
+                             (["abc", "car", "ada", "racecar", "cool"], "ada"),
+                             (["notapalindrome", "racecar"], "racecar"),
+                             (["def", "ghi"], ""),
+                             (["xngla", "e", "itrn", "y", "s", "pfp", "rfd"], "e"),
+                         ])
+def test_find_first_palindromic_str(test_input, expected):
+    assert FirstPalindrome().solve(test_input) == expected
