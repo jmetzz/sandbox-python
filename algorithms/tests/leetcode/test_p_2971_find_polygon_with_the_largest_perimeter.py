a8f75cce86e8e1fd3ef730e60d6d4b1491ac48f2
Jean Metz <jmetzz@gmail.com>
2024-02-15 10:31:31 +0100

    Add leetcode P 2971 Find polygon with the largest perimeter

--- algorithms/tests/leetcode/test_p_2971_find_polygon_with_the_largest_perimeter.py
+++ algorithms/tests/leetcode/test_p_2971_find_polygon_with_the_largest_perimeter.py
@@ -8,7 +8,7 @@ from leetcode.p_2971_find_polygon_with_the_largest_perimeter import FindPolygonW
                              ([5, 5, 5], 15),
                              ([1, 12, 1, 2, 5, 50, 3], 12),
                              ([5, 5, 50], -1),
-
+                             ([143, 24, 3, 9, 2, 18, 3, 22, 15], 96),
                          ])
 def test_find_polygon_largest_perimeter_loop(test_input, expected):
     assert FindPolygonWithLargestPerimeter().solve_with_loop(test_input) == expected
@@ -19,7 +19,7 @@ def test_find_polygon_largest_perimeter_loop(test_input, expected):
                              ([5, 5, 5], 15),
                              ([1, 12, 1, 2, 5, 50, 3], 12),
                              ([5, 5, 50], -1),
-
+                             ([143, 24, 3, 9, 2, 18, 3, 22, 15], 96),
                          ])
 def test_find_polygon_largest_perimeter_heap(test_input, expected):
     assert FindPolygonWithLargestPerimeter().solve_with_heap(test_input) == expected
@@ -30,7 +30,7 @@ def test_find_polygon_largest_perimeter_heap(test_input, expected):
                              ([5, 5, 5], 15),
                              ([1, 12, 1, 2, 5, 50, 3], 12),
                              ([5, 5, 50], -1),
-
+                             ([143, 24, 3, 9, 2, 18, 3, 22, 15], 96),
                          ])
 def test_find_polygon_largest_perimeter_reversed(test_input, expected):
     assert FindPolygonWithLargestPerimeter().solve_reversed(test_input) == expected
