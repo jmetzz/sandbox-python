6423489d58b9f2308e97d94cd2570b9f5778b88d
Jean Metz <jmetzz@gmail.com>
2024-02-25 11:51:22 +0100

    Fix import

--- src/leetcode/p_2092_find_all_people_with_secret.py
+++ src/leetcode/p_2092_find_all_people_with_secret.py
@@ -70,6 +70,8 @@ from heapq import heappop, heappush
 from math import inf
 from typing import List
 
+from algorithms.graphs import UnionFind
+
 
 class FindAllPeopleWitSecret:
     def solve_bfs(self, n: int, meetings: List[List[int]], first_person: int) -> List[int]:
@@ -245,39 +247,3 @@ class FindAllPeopleWitSecret:
 
         # Al those who are connected to 0 will know the secret
         return [i for i in range(n) if graph.connected(i, 0)]
-
-
-class UnionFind:
-    def __init__(self, nodes: int):
-        # Initialize parent and rank arrays
-        self.parent = [i for i in range(nodes)]
-        self.rank = [0] * nodes
-
-    def find(self, x: int) -> int:
-        # Find the parent of node x. Use Path Compression
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def unite(self, x: int, y: int) -> None:
-        # Unite two nodes x and y, if they are not already united
-        px = self.find(x)
-        py = self.find(y)
-        if px != py:
-            # Union by Rank Heuristic
-            if self.rank[px] > self.rank[py]:
-                self.parent[py] = px
-            elif self.rank[px] < self.rank[py]:
-                self.parent[px] = py
-            else:
-                self.parent[py] = px
-                self.rank[px] += 1
-
-    def connected(self, x: int, y: int) -> bool:
-        # Check if two nodes x and y are connected or not
-        return self.find(x) == self.find(y)
-
-    def reset(self, x: int) -> None:
-        # Reset the initial properties of node x
-        self.parent[x] = x
-        self.rank[x] = 0
