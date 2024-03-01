867a1068eeca7308b553a9191391adccc858853a
Jean Metz <jmetzz@gmail.com>
2024-03-01 19:54:01 +0100

    Add leetcode P 5 longest palindromic substring

---
+++ src/leetcode/p_5_longest_palindromic_substring.py
@@ -0,0 +1,95 @@
+"""
+5. Longest Palindromic Substring
+Medium
+
+Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+
+
+Constraints:
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+"""
+
+import heapq
+
+
+def longest_palindrome_brute_force(s: str) -> str:
+    answer = s[0]
+    n = len(s)
+    for size in range(n, 1, -1):
+        for i in range(n - size, -1, -1):
+            ss = s[i : i + size]
+            if ss == ss[::-1]:
+                answer = ss if len(ss) > len(answer) else answer
+    return answer
+
+
+def longest_palindrome_heap_brute_force(s: str) -> str:
+    substrings = set(s)
+    n = len(s)
+    for size in range(2, n + 1):
+        for i in range(n - size, -1, -1):
+            ss = s[i : i + size]
+            if ss == ss[::-1]:
+                substrings.add(s[i : i + size])
+    h = [(-len(ss), ss) for ss in substrings]
+    heapq.heapify(h)
+    _, answer = h[0]
+    return answer
+
+
+def longest_palindrome_expanding_window(sequence: str) -> str:
+    """
+    To optimize the execution we can reuse a previously computed palindrome
+    to compute a larger palindrome.
+    If “aba” is a palindrome, “XabaX” is also a palindrome.
+    Also, if “XabaZ” is not a palindrome, the other characters added to left and right
+    will not make the new string a palindrome.
+    """
+    if sequence[::-1] == sequence:
+        return sequence
+
+    answer = ""
+    for mid in range(len(sequence)):
+        # check odd length palindromes
+        even_length = backtrack_palindrome(sequence, mid, mid)
+
+        # check even length palindromes
+        odd_length = backtrack_palindrome(sequence, mid, mid + 1)
+        candidate = even_length if len(even_length) > len(odd_length) else odd_length
+        if len(candidate) > len(answer):
+            answer = candidate
+    return answer
+
+
+def backtrack_palindrome(sequence: str, left: int, right: int) -> str:
+    # expand the window while it is a palindrome
+    while left >= 0 and right < len(sequence) and sequence[left] == sequence[right]:
+        left, right = left - 1, right + 1
+    # return the string in the window
+    return sequence[left + 1 : right]
+
+
+if __name__ == "__main__":
+    inputs = [
+        # "cbbd",
+        # "babad",
+        "abbbc"
+    ]
+
+    for word in inputs:
+        # print(longest_palindrome_brute_force(word))
+        # print(longest_palindrome_heap_brute_force(word))
+        # print(longest_palindrome_expanding_window(word))
+        for i in range(len(word)):
+            print(backtrack_palindrome(word, i, i))
+        print()
---
+++ tests/leetcode/test_p_5_longest_palindromic_substring.py
@@ -0,0 +1,57 @@
+import pytest
+from leetcode.p_5_longest_palindromic_substring import (
+    backtrack_palindrome,
+    longest_palindrome_brute_force,
+    longest_palindrome_expanding_window,
+    longest_palindrome_heap_brute_force,
+)
+
+
+@pytest.mark.parametrize("input_sequence, expected", [("cbbd", "bb"), ("babad", ["bab", "aba"]), ("abbbc", ["bbb"])])
+def test_longest_palindrome_brute_force(input_sequence, expected):
+    assert longest_palindrome_brute_force(input_sequence) in expected
+
+
+@pytest.mark.parametrize("input_sequence, expected", [("cbbd", "bb"), ("babad", ["bab", "aba"]), ("abbbc", ["bbb"])])
+def test_longest_palindrome_heap_brute_force(input_sequence, expected):
+    assert longest_palindrome_heap_brute_force(input_sequence) in expected
+
+
+@pytest.mark.parametrize("input_sequence, expected", [("cbbd", "bb"), ("babad", ["bab", "aba"]), ("abbbc", ["bbb"])])
+def test_longest_palindrome_expanding_window(input_sequence, expected):
+    assert longest_palindrome_expanding_window(input_sequence) in expected
+
+
+@pytest.mark.parametrize(
+    "input_sequence, center, expected",
+    [
+        ("cbbd", 0, "c"),
+        ("cbbd", 1, "b"),
+        ("cbbd", 2, "b"),
+        ("babad", 0, "b"),
+        ("babad", 1, "bab"),
+        ("babad", 2, "aba"),
+        ("babad", 3, "a"),
+    ],
+)
+def test_backtrack_palindrome_odd_length(input_sequence, center, expected):
+    assert backtrack_palindrome(input_sequence, center, center) == expected
+
+
+@pytest.mark.parametrize(
+    "input_sequence, center, expected",
+    [
+        ("cbbd", 0, ""),
+        ("cbbd", 1, "bb"),
+        ("cbbd", 2, ""),
+        ("babad", 0, ""),
+        ("babad", 1, ""),
+        ("babad", 2, ""),
+        ("babad", 3, ""),
+        ("abbad", 0, ""),
+        ("abbad", 1, "abba"),
+        ("abbad", 2, ""),
+    ],
+)
+def test_backtrack_palindrome_even_length(input_sequence, center, expected):
+    assert backtrack_palindrome(input_sequence, center, center + 1) == expected
