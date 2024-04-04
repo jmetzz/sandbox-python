e33a11cba06f4d9b457127b33040e7280c5c9ed5
Jean Metz <jmetzz@gmail.com>
2024-04-04 17:55:16 +0200

    Add leetcode solutions for P. 2542 and P. 1383

---
+++ tests/leetcode/test_p_1383_maximum_performance_team.py
@@ -0,0 +1,15 @@
+import pytest
+from leetcode.p_1383_maximum_performance_team import max_performance
+
+
+@pytest.mark.parametrize(
+    "nums1, nums2, k, expected",
+    [
+        ([2, 10, 3, 1, 5, 8], [5, 4, 3, 9, 7, 2], 2, 60),
+        ([2, 10, 3, 1, 5, 8], [5, 4, 3, 9, 7, 2], 3, 68),
+        ([2, 10, 3, 1, 5, 8], [5, 4, 3, 9, 7, 2], 4, 72),
+        ([2, 8, 2], [2, 7, 1], 2, 56),
+    ],
+)
+def test_max_performance(nums1, nums2, k, expected):
+    assert max_performance(nums1, nums2, k) == expected
---
+++ tests/leetcode/test_p_2542_maximum_subsequence_score.py
@@ -0,0 +1,9 @@
+import pytest
+from leetcode.p_2542_maximum_subsequence_score import max_score
+
+
+@pytest.mark.parametrize(
+    "nums1, nums2, k, expected", [([1, 3, 3, 2], [2, 1, 3, 4], 3, 12), ([4, 2, 3, 1, 1], [7, 5, 10, 9, 6], 1, 30)]
+)
+def test_max_score(nums1, nums2, k, expected):
+    assert max_score(nums1, nums2, k) == expected
