b058c888912ba02ee558602b709df1f7f8c03a2d
Jean Metz <jmetzz@gmail.com>
2024-04-29 12:10:17 +0200

    Add leetcode solution

---
+++ tests/leetcode/test_p_2997_min_num_operations_to_make_array_xor_eq_k.py
@@ -0,0 +1,8 @@
+import pytest
+from leetcode.p_2997_min_num_operations_to_make_array_xor_eq_k import min_operations, min_perations_iterative
+
+
+@pytest.mark.parametrize("func", [min_operations, min_perations_iterative])
+@pytest.mark.parametrize("input_arr, k, expected", [([2, 1, 3, 4], 1, 2), ([2, 0, 2, 0], 0, 0)])
+def test_min_operations(func, input_arr, k, expected):
+    assert func(input_arr, k) == expected
