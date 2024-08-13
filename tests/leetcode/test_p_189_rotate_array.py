fdc6d4484426138fb893753108e116c40a25dbf5
Jean Metz <jmetzz@gmail.com>
2024-08-13 08:25:03 +0200

    fix unit tests

--- tests/leetcode/test_p_189_rotate_array.py
+++ tests/leetcode/test_p_189_rotate_array.py
@@ -8,5 +8,6 @@ from leetcode.p_189_rotate_array import rotate_inplace_1, rotate_inplace_2
     [([1, 2, 3, 4, 5, 6, 7], 3, [5, 6, 7, 1, 2, 3, 4]), ([-1, -100, 3, 99], 2, [3, 99, -1, -100])],
 )
 def test_function_name(func, input_data, k, expected):
-    func(input_data, k)
-    assert input_data == expected
+    arr = input_data[:]
+    func(arr, k)
+    assert arr == expected
--- tests/leetcode/test_p_88_merge_sorted_array.py
+++ tests/leetcode/test_p_88_merge_sorted_array.py
@@ -9,13 +9,15 @@ test_cases = [
 ]
 
 
-@pytest.mark.parametrize("arr1, m, arr2, n, expected", test_cases)
-def test_merge_inplace(arr1, m, arr2, n, expected):
-    merge_inplace(arr1, m, arr2, n)
+@pytest.mark.parametrize("input_arr1, m, input_arr2, n, expected", test_cases)
+def test_merge_inplace(input_arr1, m, input_arr2, n, expected):
+    arr1 = input_arr1[:]  # to avoid side-effects from inplace changes
+    merge_inplace(arr1, m, input_arr2, n)
     assert arr1 == expected
 
 
-@pytest.mark.parametrize("arr1, m, arr2, n, expected", test_cases)
-def test_merge_inplace_2(arr1, m, arr2, n, expected):
-    merge_inplace_2(arr1, m, arr2, n)
+@pytest.mark.parametrize("input_arr1, m, input_arr2, n, expected", test_cases)
+def test_merge_inplace_2(input_arr1, m, input_arr2, n, expected):
+    arr1 = input_arr1[:]  # to avoid side-effects from inplace changes
+    merge_inplace_2(arr1, m, input_arr2, n)
     assert arr1 == expected
