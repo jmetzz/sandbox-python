62a0243181e28fad78a20d44eac13cbbc11039f3
Jean Metz <jmetzz@gmail.com>
2024-08-23 18:32:31 +0200

    fix: generate leetcode file

--- scripts/generate_leetcode_links_md.py
+++ scripts/generate_leetcode_links_md.py
@@ -1,12 +1,16 @@
+import json
+import logging
 import re
 from pathlib import Path
 from typing import List, Tuple
 
 # Compile the regular expression once and use it globally
-leetcode_url_pattern = re.compile(r"https://leetcode\.\w+/problems/[\w-]+/")
+leetcode_url_pattern = re.compile(r"https://leetcode\.\w+/problems/[\w-]+/?")
+
+LOGGER = logging.getLogger()
 
 
-def extract_leetcode_links(file_path: Path) -> Tuple[str, str]:
+def extract_leetcode_links(file_path: Path) -> Tuple[str, str, int]:
     """
     Extracts the first LeetCode URL from the initial docstring of a Python file and
     generates a tuple containing the URL and a descriptive name based on the file naming
@@ -24,7 +28,7 @@ def extract_leetcode_links(file_path: Path) -> Tuple[str, str]:
     if len(file_name_parts) < 3 or file_name_parts[0] != "p":
         return ("", "")  # Return empty if file doesn't match the naming convention
 
-    problem_number = file_name_parts[1]
+    problem_number = int(file_name_parts[1])
     problem_title = " ".join(file_name_parts[2:]).title()
 
     with open(file_path, encoding="utf-8") as file:
@@ -33,12 +37,12 @@ def extract_leetcode_links(file_path: Path) -> Tuple[str, str]:
         if match:
             url = match.group(0)
             descriptive_name = f"Problem {problem_number}: {problem_title}"
-            return (url, descriptive_name)
+            return (url, descriptive_name, problem_number)
 
-    return ("", "")  # Return empty if no URL is found
+    return (None, None, -1)  # Return empty if no URL is found
 
 
-def scan_directory_for_links(directory: Path) -> List[Tuple[str, str]]:
+def scan_directory_for_links(directory: Path) -> List[Tuple[str, str, int]]:
     """
     Scans a directory recursively for Python files, extracting the first LeetCode URL
     from their initial docstrings, along with generating a descriptive name based on the
@@ -52,14 +56,18 @@ def scan_directory_for_links(directory: Path) -> List[Tuple[str, str]]:
         their descriptive names. Empty tuples are filtered out.
     """
     links = []
+    missing_links = []
     for file in directory.rglob("p_*.py"):  # Only include files matching the naming convention
-        link = extract_leetcode_links(file)
-        if link[0]:  # Add link if it's not empty
-            links.append(link)
-    return sorted(links, key=lambda x: x[1])  # Sort by descriptive name
+        link, description, problem_id = extract_leetcode_links(file)
+        if link:  # Add link if it's not empty
+            links.append((link, description, problem_id))
+        else:
+            missing_links.append(file.stem)
+
+    return links, missing_links
 
 
-def write_links_to_markdown(links: List[Tuple[str, str]], output_file: Path) -> None:
+def write_links_to_markdown(links: List[Tuple[str, str, int]], output_file: Path) -> None:
     """
     Writes a list of LeetCode URLs and their descriptive names to a Markdown file,
     formatted as a list.
@@ -70,12 +78,14 @@ def write_links_to_markdown(links: List[Tuple[str, str]], output_file: Path) -> 
     """
     with open(output_file, "w", encoding="utf-8") as md_file:
         md_file.write("# LeetCode Links\n\n")
-        for url, name in links:
+        for url, name, _ in links:
             md_file.write(f"- [{name}]({url})\n")
 
 
 if __name__ == "__main__":
     subdirectory = Path("src/leetcode")  # Adjust this path
     output_file = Path("LeetCode_Links.md")  # Output Markdown file
-    links = scan_directory_for_links(subdirectory)
+    links, misses = scan_directory_for_links(subdirectory)
+    links.sort(key=lambda x: x[2])  # Sort by descriptive name
     write_links_to_markdown(links, output_file)
+    LOGGER.warning("Files missing URL: %s", json.dumps(sorted(misses), indent=4))
