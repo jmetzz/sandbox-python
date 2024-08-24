5f7d86af85e77ebe84cb1ab67663e384e043da47
Jean Metz <jmetzz@gmail.com>
2024-08-24 12:48:25 +0200

    chore(clean up)

--- src/leetcode/p_31_next_permutation.py
+++ src/leetcode/p_31_next_permutation.py
@@ -42,46 +42,117 @@ Constraints:
 """
 
 
+def next_permutation_review(nums: list[int]) -> None:
+    """Do not return anything, modify nums in-place instead
+
+    Observations:
+    1. a decreasing subsequence (from left to right) cannot have
+       a next lexicographical sorted permutation
+       Example: in [... 4, 7, 6, 5, 3, 1] from 7 onwards,
+       it is not possible to permute elements and make
+       the sequence lexicographical bigger
+    2. if an item interrupts the decreasing sequence, it is a pivot
+       we can use to swap with another element (to the right)
+       that would make the the lexicographical order higher.
+       Example: in [...8, 4, 7, 6, 5, 3, 1];
+       4 and 7 are the pair of elements that breaks the decreasing sequence.
+       Then, 4 is our select pivot element.
+    3. Since we interested in the next permutation (in the lexicographical order)
+       and the subsequence to the right of the pivot element
+       is in lexicographical order, we can explore the array
+       from right to left to find the target element to swap with
+       the pivot. The target element is the one that is just larger
+       than the pivot.
+        Example:
+    sequence: [1, 5, 8, 4, 7, 6, 5, 3, 1]
+                           |-----> decreasing subsequence
+                        ^
+                        |--- pivot: the element just before the decreasing subsequence
+    """
+
+    def _reverse_inplace(start):
+        left, right = start, n - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+    n = len(nums)
+    pivot = n - 2
+    # find the first element that breaks the decreasing sequence
+    while pivot >= 0 and nums[pivot] >= nums[pivot + 1]:
+        pivot -= 1
+
+    # [1, 5, 8, 4, 7, 6, 5, 3, 1]
+    #           L
+
+    if pivot >= 0:
+        # find the "just larger" than nums[left] element to the right of left
+        swap_target = n - 1
+        while nums[pivot] >= nums[swap_target]:
+            swap_target -= 1
+        # [1, 5, 8, 4, 7, 6, 5, 3, 1]
+        #           L        R
+
+        # swap places
+        nums[pivot], nums[swap_target] = nums[swap_target], nums[pivot]
+        # [1, 5, 8, 5, 7, 6, 4, 3, 1]
+        #           L        R
+
+    # reverse the elements from left + 1
+    _reverse_inplace(pivot + 1)
+
+
 def next_permutation(nums: list[int], verbose: bool = False) -> None:
-    """Do not return anything, modify nums in-place instead."""
+    """Do not return anything, modify nums in-place instead.
+
+    any given sequence that is in descending order,
+    can not have a next larger permutation.
+    Example: [9, 5, 4, 3, 1]
+    while [5, 4, 9, 3, 1] has a element that
+    breaks the decreasing order (e.g. 9)
+    """
 
     def _reverse_in_place(start):
-        i, j = start, n - 1
-        while i < j:
-            nums[i], nums[j] = nums[j], nums[i]
-            i += 1
-            j -= 1
-
-    # any given sequence that is in descending order,
-    # can not have a next larger permutation.
-    # Example: [9, 5, 4, 3, 1]
-    #     while [5, 4, 9, 3, 1] has a element that
-    #     breaks the decreasing order (e.g. 9)
+        left, right = start, n - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
 
     # Thus, from right to left find the first 'decreasing' element index
     n = len(nums)
-    left = n - 2
-    while left >= 0 and nums[left] >= nums[left + 1]:
-        left -= 1
+    pivot = n - 2
+    while pivot >= 0 and nums[pivot] >= nums[pivot + 1]:
+        pivot -= 1
 
-    if left >= 0:
-        # find the next number just larget than nums[left]
+    if pivot >= 0:
+        # find the next number just larger than nums[left]
         # on the right side of nums[left]
-        right = n - 1
-        while nums[left] >= nums[right]:
-            right -= 1
+        swap_target = n - 1
+        while nums[pivot] >= nums[swap_target]:
+            swap_target -= 1
         # swap them
-        nums[left], nums[right] = nums[right], nums[left]
+        nums[pivot], nums[swap_target] = nums[swap_target], nums[pivot]
 
     # reverse the reminder of the array
-    _reverse_in_place(start=left + 1)
+    _reverse_in_place(start=pivot + 1)
 
     if verbose:
         print(nums)
 
 
 if __name__ == "__main__":
-    next_permutation([1, 2, 3])  # [1, 3, 2]
-    next_permutation([3, 2, 1])  # [1, 2, 3]
-    next_permutation([1, 1, 5])  # [1, 5, 1]
-    next_permutation([1, 2])  # [2,1]
+    inputs = [
+        [1, 2, 3],  # [1, 3, 2]
+        [3, 2, 1],  # [1, 2, 3]
+        [1, 1, 5],  # [1, 5, 1]
+        [1, 2],  # [2,1]
+    ]
+    functions = [next_permutation, next_permutation_review]
+    for input_arr in inputs:
+        for func in functions:
+            arr = input_arr[:]
+            func(arr)
+            print(arr)
+        print()
--- tests/leetcode/test_p_31_next_permutation.py
+++ tests/leetcode/test_p_31_next_permutation.py
@@ -1,6 +1,6 @@
 import pytest
 
-from leetcode.p_31_next_permutation import next_permutation
+from leetcode.p_31_next_permutation import next_permutation, next_permutation_review
 
 
 @pytest.mark.parametrize(
@@ -12,66 +12,17 @@ def test_next_permutation(input_arr, expected):
     assert input_arr == expected
 
 
-def next_permutation_review(nums: list[int]) -> None:
-    """Do not return anything, modify nums in-place instead."""
-    # Observations:
-    # 1. a decreasing subsequence (from left to right) cannot have a next lexicographical sorted permutation
-    # 2. if an item interrupts the decreasing sequence, it is a pivot we can use to swap with
-    #    another element that would make the the next lexicographical order.
-    #    The target element is the one that is just larger than this element.
-    # 3. Since we interested in the next permutation (in the lexicographical order)
-    #    and the subsequence to the right of the element identified in step 1
-    #    is in lexicographical order, we can explore the array from right to left
-    #    to find the target element to swap.
-
-    # sequence: [1, 5, 8, 4, 7, 6, 5, 3, 1]
-    #                        |-----> decreasing subsequence
-    #                     ^
-    #                     |--- the element just before the decreasing subsequence
-
-    def _reverse_inplace(start):
-        left, right = start, n - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
-
-    n = len(nums)
-    left = n - 2
-
-    # find the first element that breaks the decreasing sequence
-    while left >= 0 and nums[left] >= nums[left + 1]:
-        left -= 1
-
-    # [1, 5, 8, 4, 7, 6, 5, 3, 1]
-    #           L
-
-    if left > 0:
-        # find the "just larger" than nums[left] element to the right of left
-        right = n - 1
-        while nums[left] >= nums[right]:
-            right -= 1
-        # [1, 5, 8, 4, 7, 6, 5, 3, 1]
-        #           L        R
-
-        # swap places
-        nums[left], nums[right] = nums[right], nums[left]
-        # [1, 5, 8, 5, 7, 6, 4, 3, 1]
-        #           L        R
-
-        # reverse the elements from left + 1
-        _reverse_inplace(left + 1)
-
-
 @pytest.mark.parametrize(
     "input_arr, expected",
     [
-        # ([1, 2, 3], [1, 3, 2]), ([3, 2, 1], [1, 2, 3]), ([1, 1, 5], [1, 5, 1]), ([1, 2], [2, 1])
-        ([1, 5, 8, 4, 7, 6, 5, 3, 1], [1, 5, 8, 5, 1, 3, 4, 6, 7])
+        ([1, 2, 3], [1, 3, 2]),
+        ([3, 2, 1], [1, 2, 3]),
+        ([1, 1, 5], [1, 5, 1]),
+        ([1, 2], [2, 1]),
+        ([1, 5, 8, 4, 7, 6, 5, 3, 1], [1, 5, 8, 5, 1, 3, 4, 6, 7]),
     ],
 )
 def test_next_permutation_review(input_arr, expected):
     # the function under tests perform in_place operation
-    # input_arr = [1, 5, 8, 4, 7, 6, 5, 3, 1]
     next_permutation_review(input_arr)
     assert input_arr == expected
